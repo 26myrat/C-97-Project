317886a4d3b97ae0bb39b2a1b1501786d4e042f7
26myrat <71564716+26myrat@users.noreply.github.com>
2021-11-21 09:41:45 -0800

    Add files via upload

--- guessingGame.py
+++ guessingGame.py
@@ -1,14 +1,15 @@
-
-number= input("Guess a number 1-9")
-print('Enter guess:', number)
-if (pocketMoney<5):
-    print("Guess higher!")
-elif(pocketMoney>6 and pocketMoney<10):
-    print("Guess Lower!")
-else:
-    print("You Won!!")
-
-chances<5
-
+import random
+rand= random.randint(1,9)
+chances=0
+while(chances<5):
+    number= input("Guess a number 1-9")
+    print('Enter guess:', number)
+    if (number<rand):
+        print("Guess higher!")
+    elif(number>rand):
+        print("Guess Lower!")
+    else:
+        print("You Won!!")
+    chances+=1
 if not chances <5:
     print("You lose! The number is", number)
